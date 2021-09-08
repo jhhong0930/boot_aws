85362d317def2b161ab8cd67c6d6cad960ecdbd2
Joe <jhhong0930@gmail.com>
2021-09-08 11:15:36 +0900

    3.3 - Spring Data JPA 테스트 코드 작성하기

---
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자
+@Entity // 테이블과 링크될 클래스
+public class Posts {
+
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // pk 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스 생성
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
